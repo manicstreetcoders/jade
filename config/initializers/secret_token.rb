6ead40aca6a5cd3bb144ece624b7f8480507830e
manicstreetcoders <manicstreetcoders@gmail.com>
2013-06-05 11:00:25 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jade::Application.config.secret_token = 'f9fa5bfa3c6b751113ebc7df797c84442197784006dd5da4ee145df31f194c8af80ad98e8e4ce456d006e49f69b0243155646885b86dd0b3a7a486c2b34d4699'
